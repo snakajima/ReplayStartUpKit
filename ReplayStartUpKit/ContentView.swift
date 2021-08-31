bbfef46b5f31669b4a7dfe70790c43bec4095b18
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-08-31 15:26:59 -0700

    Initial Commit

---
+++ ReplayStartUpKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReplayStartUpKit
+//
+//  Created by SATOSHI NAKAJIMA on 8/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
